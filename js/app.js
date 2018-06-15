93b6b71b656d3f8894a9f2ff28d4f4791d372103
Sally Morningstar <sally.morningstar@icloud.com>
2018-06-15 18:12:08 -0400

    Add test click event -> changes card background to black

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 /*
  * Create a list that holds all of your cards
  */
-
+const options = document.getElementsByClassName('card');
+console.log(options);
 
 /*
  * Display the cards on the page
@@ -25,10 +26,15 @@ function shuffle(array) {
     return array;
 }
 
+/* set up the event listener for a card. If a card is clicked: */
+ document.addEventListener('click', function(event) {
+ $('ul').on('click','.card', function() {
+ $(this).css('background-color', '#000000');
+});
+});
 
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
+
+ /*  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
