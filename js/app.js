52867dcd231619ae44e0c2a3279eccab53a54db1
Sally Morningstar <sally.morningstar@icloud.com>
2018-06-20 17:41:11 -0400

    Add card comparison, lock matches, & return mismatched cards

--- js/app.js
+++ js/app.js
@@ -32,18 +32,49 @@ shuffle(cards);
      function that you call from this one) */
 
 // set up the check to see if two cards are revealed
-const turned = document.getElementsByClassName('open show');
 
 
 // set up the function to reveal a card
 const look = function() {
-     $(this).toggleClass('open show');
-     console.log(turned.length);     
+     $(this).toggleClass('open');
+     $(this).toggleClass('show');
+};
+
+const check = function() {
+const turned = document.getElementsByClassName('open');
+     console.log(turned.length);
+     if(turned.length === 2) {
+          let card1 = document.getElementsByClassName('open').item(0);
+          let card1Style = card1.firstElementChild.classList.item(1);
+          let card2 = document.getElementsByClassName('open').item(1);
+          let card2Style = card2.firstElementChild.classList.item(1);
+          console.log(card1Style);
+          console.log(card2Style);
+          if(card1Style === card2Style) {
+               card1.classList.remove('open','show');
+               card2.classList.remove('open','show');
+               card1.classList.add('match');
+               card2.classList.add('match');
+               card1 = '';
+               card2= '';
+               card1Style = '';
+               card2Style = '';
+          } else {
+          console.log("fail....try again");
+          setTimeout(function() {
+          card1.classList.remove('open','show');
+          card2.classList.remove('open','show');
+          }, 2000);
+     }} else {
+          console.log("Please choose a card");
+     }
 };
 
 // set up the function to respond to user selection
 const select = function(event) {
      $('ul').on('click', 'li', look);
+     setTimeout(function() {
+          check()}, 2000);
 };
 
 // Listen for a click on a card
