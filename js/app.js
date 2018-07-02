1f4184397c4f620b953681fbb66e0c6158deef8c
Sally Morningstar <sally.morningstar@icloud.com>
2018-07-01 21:59:33 -0400

    Add win check and announcement

--- js/app.js
+++ js/app.js
@@ -14,55 +14,17 @@ let cardPic = ['diamond','diamond',
 // Define variables
 const deck = document.getElementById('deck');
 const turned = document.getElementsByClassName('open');
-let cards = document.querySelectorAll('.card');
-let moves = 3;
-let starScore = 3;
+const cards = document.querySelectorAll('.card');
+let cardMatch = 0;
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+const moveCounter = document.getElementById('moves');
+     let moves = 3;
+     let starScore = 3;
+const redeal = document.getElementById('deal');
 
-// Shuffle function from http://stackoverflow.com/a/2450976
-function shuffle(cards) {
-    let currentIndex = cardPic.length, temporaryValue, randomIndex;
-    while (currentIndex !== 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex -= 1;
-        temporaryValue = cards[currentIndex];
-        cards[currentIndex] = cards[randomIndex];
-        cards[randomIndex] = temporaryValue;
-    }
-    return cards;
-}
+// Define functions
 
-// shuffle the cards prior to di
-cardPic = shuffle(cardPic);
-
-// now, deal the cards
-cardPic.forEach(function(item, index, array) {
-     // for each image, creat a card element
-     const cardEl = document.createElement('li');
-     // for each card element, add the class
-     cardEl.classList.add("card");
-     // add the card to the page
-     deck.append(cardEl);
-
-     // for each card, create the image element
-     const iEl = document.createElement('i');
-     // create the class name for each image
-     const iClass =  cardPic[index];
-     // for each image, add the class
-     iEl.classList.add("fa","fa-"+iClass);
-     // add the image to the card created above
-     cardEl.append(iEl);
-});
-
-// set the initial score Paneel
 // -------- stars --------
-
 let stars = document.getElementById('stars');
 const starkeeper = function(count) {
      let i=0;
@@ -75,11 +37,62 @@ const starkeeper = function(count) {
           ++i;
      };
 };
-starkeeper(starScore);
 
-// ------- numeric move counter -------
-const moveCounter = document.getElementById('moves');
-moveCounter.prepend(moves);
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+const shuffle = function shuffle(cards) {
+    let currentIndex = cardPic.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = cards[currentIndex];
+        cards[currentIndex] = cards[randomIndex];
+        cards[randomIndex] = temporaryValue;
+    }
+    return cards;
+};
+
+const deal = function() {
+     while(deck.hasChildNodes()) {
+          deck.removeChild(deck.firstChild);
+     };
+     while(stars.hasChildNodes()) {
+          stars.removeChild(stars.firstChild);
+     };
+     moves = 3;
+     starscore = 3;
+     // shuffle the cards prior to deal
+     cardPic = shuffle(cardPic);
+
+     // now, deal the cards
+     cardPic.forEach(function(item, index, array) {
+          // for each image, creat a card element
+          const cardEl = document.createElement('li');
+          // for each card element, add the class
+          cardEl.classList.add("card");
+          // add the card to the page
+          deck.append(cardEl);
+
+          // for each card, create the image element
+          const iEl = document.createElement('i');
+          // create the class name for each image
+          const iClass =  cardPic[index];
+          // for each image, add the class
+          iEl.classList.add("fa","fa-"+iClass);
+          // add the image to the card created above
+          cardEl.append(iEl);
+     });
+     // ------- set star counter -------
+     starkeeper(starScore);
+     // ------- numeric move counter -------
+     moveCounter.innerHTML = moves;
+};
 
 // reveal a card
 const look = function(guess) {
@@ -110,7 +123,13 @@ const check = function() {
                card1Style = '';
                card2Style = '';
           }, 600);
-     } else {  // if not a match, reset the cards after a pause to view
+          ++ cardMatch;
+          let checkWin = document.getElementsByClassName('card');
+          if(cardMatch ===(checkWin.length/2)) {
+               alert("YOU WON!");
+          };
+     } else {
+          // if not a match, reset the cards after a pause to view
           setTimeout(function() {
                card1.classList.add('close');
                card2.classList.add('close');
@@ -150,7 +169,10 @@ const select = function(guess) {
 - display the card's symbol (put this functionality in another
 function that you call from this one) */
 
-// when user clicks on a card, launch select() on the guess to look & check the card
+// deal the cards to start the game
+deal();
+
+// add listener for card selection
 deck.addEventListener('click', function(e) {
      const guess = e.target;
      if(guess.classList == 'card') {
@@ -158,7 +180,10 @@ deck.addEventListener('click', function(e) {
      }
 }, true);
 
-
+// add listener for redeal
+redeal.addEventListener('click', function() {
+     deal();
+});
  /*  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
